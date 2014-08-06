a7d0ec1dd817aeedf16d334d9a9d74e583d2f443
vpestov <valera.hilt@yandex.com>
2014-08-06 21:13:57 +0300

    initial commit

---
+++ src/main/java/com/springAppStore/controllers/MainPageController.java
@@ -0,0 +1,7 @@
+package com.springAppStore.controllers;
+
+/**
+ * Created by valeriy on 8/6/14.
+ */
+public class MainPageController {
+}
---
+++ src/main/java/com/springAppStore/services/UserService.java
@@ -0,0 +1,7 @@
+package com.springAppStore.services;
+
+/**
+ * Created by valeriy on 8/6/14.
+ */
+public class UserService {
+}
